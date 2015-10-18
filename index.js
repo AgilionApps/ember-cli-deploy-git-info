04c8e473e3c8a7ee986caa178c7d7fe2ba23da61
Alan Peabody <gapeabody@gmail.com>
2015-10-17 22:03:52 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+/* jshint node: true */
+'use strict';
+
+var DeployPluginBase = require('ember-cli-deploy-plugin');
+
+module.exports = {
+  name: 'ember-cli-deploy-plugin-git-info'
+
+  createDeployPlugin: function(options) {
+    var DeployPlugin = DeployPluginBase.extend({
+      name: options.name,
+      defaultConfig: {},
+      requiredConfig: [],
+
+      setup: function(context) {
+      }
+    });
+
+    return new DeployPlugin();
+  }
+};
